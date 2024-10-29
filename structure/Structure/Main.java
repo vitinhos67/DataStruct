510183aaef0455a1fa9ec09db2fc896337236926
Victor Santos da Silva <santtosvictor42@gmail.com>
2024-10-29 19:29:23 -0300

    ref: removido comentario

--- structure/Structure/Main.java
+++ structure/Structure/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int numElements = 1000;  // Número de elementos para inserir
-
+        int numElements = 1000;
         System.out.println("Comparando tempos de inserção para " + numElements + " elementos:");
         DataStructureComparison.compareStructures(numElements);
     }
